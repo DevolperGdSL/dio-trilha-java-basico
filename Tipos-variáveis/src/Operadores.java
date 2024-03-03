b41d69fd37d0b89d0c4373408341b26bb85dc373
https://github.com/DevolperGdSL/dio-java-basico.git <Dev.gabriel.lopes@outlook.com>
2024-03-03 11:50:16 -0400

    atualização

---
+++ Tipos-variáveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Nome"+ " " + "+" + " " + "Sobrenome";
+        System.out.println(nomeCompleto);
+        int numero = 5;
+        numero = -numero;
+        int i;
+        for(i=0;i<4;i++){
+            System.out.println(i);
+        }
+        System.out.println(numero);
+        int a = 4;
+        int b = 5;
+        boolean resultado = a==b ? true: false;
+        System.out.println(resultado);
+    }
+}
--- Tipos-variáveis/src/Tiposvariaveis.java
+++ Tipos-variáveis/src/Tiposvariaveis.java
@@ -1,5 +1,13 @@
 public class Tiposvariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int ano = 2024;
+        double peso = 60.5;
+        String texto = "Hello World";
+        boolean teste = false;
+        System.out.println(texto);
+        System.out.println(peso);
+        System.out.println(ano);
+        System.out.println(teste);
+       
     }
 }
