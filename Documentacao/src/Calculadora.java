9e86d696dcb24b186ba96ef3b07dc4dbc6072858
https://github.com/DevolperGdSL/dio-java-basico.git <Dev.gabriel.lopes@outlook.com>
2024-03-03 18:00:17 -0400

    doc

---
+++ Documentacao/src/Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculadora {
+    /**
+     * Faz a soma entre dois paramentros e retorna um inteiro
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+    /**
+     * Faz a multiplicação entre dois paramentros e retorna um inteiro
+     */
+    public int multiplicar(int numeroUm, int numeroDois){
+        return numeroUm * numeroDois;
+    }
+    /**
+     * Faz a divisão entre dois paramentros e retorna um inteiro
+     */
+    public int dividir(int numeroUm, int numeroDois){
+        return numeroUm / numeroDois;
+    }
+    /**
+     * Faz a subtração entre dois paramentros e retorna um inteiro
+     */
+    public int subtrair(int numeroUm, int numeroDois){
+        return numeroUm - numeroDois;
+    }
+}
