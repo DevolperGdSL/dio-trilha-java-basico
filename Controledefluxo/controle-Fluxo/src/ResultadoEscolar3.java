f32e06f5771924049f5e9253ce5915dbdc72a942
https://github.com/DevolperGdSL/dio-java-basico.git <Dev.gabriel.lopes@outlook.com>
2024-03-04 17:26:32 -0400

    estruturas de fluxo

---
+++ Controledefluxo/controle-Fluxo/src/ResultadoEscolar3.java
@@ -0,0 +1,6 @@
+public class ResultadoEscolar3 {
+    public static void main(String[] args) throws Exception {
+        int nota = 6;
+        System.out.println(nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação":"Reprovado");
+    }
+}
